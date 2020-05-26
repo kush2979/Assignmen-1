55d12783cf4bb1eebc58a2d0a3078a746dfb9bd3
kush2979 <65900657+kush2979@users.noreply.github.com>
2020-05-27 02:21:47 +0530

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Data Extraction</title>
+<style type="text/css">
+table {
+  border-collapse: collapse;
+}
+
+table, th, td {
+  border: 2px solid #d96459;
+}
+table {
+	border: 2px solid #d96459;
+	width: 100%;
+	color: black;
+	font-family: minispace;
+	font-size: 15px;
+	text--align: center;
+}
+th, td {
+  padding: 12px;
+  text--align: center;
+  
+  
+}
+tr:nth-child(even) {background-color: #f2f2f2;}
+th{
+	background-color: black;
+	color: white;
+}
+</style>
+</head>
+<body>
+
+<input type="text" name="" id="myInput" placeholder="names..." onkeyup="searchFun()">
+<table>
+<tr>
+<table id="myTable">
+<tr class="header">
+<th>id</th>
+<th>First_Name</th>
+<th>Last_Name</th>
+<th>Birthday_day</th>
+<th>Birthday_Month</th>
+<th>Birthday_Year</th>
+<th>Email_Id</th>
+<th>Mobile_Number</th>
+<th>Gender</th>
+<th>Address</th>
+<th>City</th>
+<th>Pin_Code</th>
+<th>State</th>
+<th>Contry_Name</th>
+<th>ClassX_Board</th>
+<th>ClassX_Percentage</th>
+<th>ClassX_YrOfPassing</th>
+<th>ClassXII_Board</th>
+<th>ClassXII_Percentage</th>
+<th>ClassXII_YrOfPassing</th>
+<th>Graduation_Board</th>
+<th>Graduation_Percentage</th>
+<th>Graduation_YrOfPassing</th>
+<th>Masters_Board</th>
+<th>Masters_Percentage</th>
+<th>Masters_YrOfPassing</th>
+<th>Course</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "pagedb");
+if ($conn-> connect_error) {
+die("Connection Failed:".$conn-> connect_error);
+}
+
+$sql = "SELECT id, First_Name, Last_Name, Birthday_day, Birthday_Month, Birthday_Year, Email_Id, Mobile_Number, Gender, Address, City, Pin_Code, State, Contry_Name, ClassX_Board, ClassX_Percentage, ClassX_YrOfPassing, ClassXII_Board, ClassXII_Percentage, ClassXII_YrOfPassing, Graduation_Board, Graduation_Percentage, Graduation_YrOfPassing, Masters_Board, Masters_Percentage, Masters_YrOfPassing, Course from registration";
+$result = $conn->query($sql);
+
+if ($result->num_rows>0){
+while ($row = $result-> fetch_assoc()) {
+echo "<tr><td>". $row["id"] ."</td><td>". $row["First_Name"] ."</td><td>". $row["Last_Name"] ."</td><td>". $row["Birthday_day"] ."</td><td>". $row["Birthday_Month"] ."</td><td>". $row["Birthday_Year"] ."</td><td>". $row["Email_Id"] ."</td><td>". $row["Mobile_Number"] ."</td><td>". $row["Gender"] ."</td><td>". $row["Address"] ."</td><td>". $row["City"] ."</td><td>". $row["Pin_Code"] ."</td><td>". $row["State"] ."</td><td>". $row["Contry_Name"] ."</td><td>". $row["ClassX_Board"] ."</td><td>". $row["ClassX_Percentage"] ."</td><td>". $row["ClassX_YrOfPassing"] ."</td><td>". $row["ClassXII_Board"] ."</td><td>". $row["ClassXII_Percentage"] ."</td><td>". $row["ClassXII_YrOfPassing"] ."</td><td>". $row["Graduation_Board"] ."</td><td>". $row["Graduation_Percentage"] ."</td><td>". $row["Graduation_YrOfPassing"] ."</td><td>". $row["Masters_Board"] ."</td><td>". $row["Masters_Percentage"] ."</td><td>". $row["Masters_YrOfPassing"] ."</td><td>". $row["Course"] ."</td><td>";
+}
+echo "</table>";
+}
+else {
+echo "0 result";
+}
+
+$conn-> close();
+?>
+</table>
+
+<script>
+
+const searcFun = () =>{
+	let filter = document.getElementById('myInput').value.toUpperCase();
+	
+	let myTable = document.getElementById('myTable');
+	
+	let tr = myTable.getElementsByTagName('tr');
+	
+	for(var i=0; i<tr.length; i++){
+		 let td = tr[i].getElementsByTagName('td')[1];
+		  if(td){
+			 let textvalue = td.textContent || td.innerHTML;
+			 
+			 if(textvalue.toUpperCase().indexOf(filter) > -1){
+				  tr[i].style.display = "";
+			 } else{
+				 tr[i].style.display = "none";
+			 }
+		  }
+	} 
+}
+
+
+</script>    
+</body>
+</html>
